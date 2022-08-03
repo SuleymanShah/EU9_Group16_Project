fc65a67867ac6d84b6f672bfaddd6fde8522e997
SuleymanShah <snalbant3399@gmail.com>
2022-08-03 19:54:09 +0100

    Some updates are made

--- src/featureA/Test.java
+++ src/featureA/Test.java
@@ -3,8 +3,9 @@ package featureA;
 public class Test {
     public static void main(String[] args) {
 
-        System.out.println("Hello this is Group_16 project space.");
+        System.out.println("Hello! this is Group_16 project space.");
 
+        System.out.println("Where our journey starts here...");
 
 
     }
