786d6fdbf1ca18bbeee309561e6327de4d5eba86
feridekaraca <omersaid05@hotmail.com>
2022-08-03 22:21:26 +0100

    updated

--- src/featureFeride/FerideSaglam.java
+++ src/featureFeride/FerideSaglam.java
@@ -2,6 +2,7 @@ package featureFeride;
 
 public class FerideSaglam {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello world" +
+                "");
     }
 }
